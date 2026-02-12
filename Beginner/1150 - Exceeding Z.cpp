6c0778c0a4e283e479760feb3797387af07d704f
ErickMendes21 <erickmendesrocha15@gmail.com>
2026-02-11 22:58:18 -0300

    1150

---
+++ Beginner/1150 - Exceeding Z.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int X, Z, S = 0, aux = 0;
+    cin >> X;
+    cin >> Z;
+    while (Z <= X) {
+        cin >> Z;
+    }
+    
+    for(int i = X; i <= Z; i++) {
+        S = S + i;
+        aux++;
+        if (S >= Z) {
+            break;
+        }
+    }
+    cout << aux << endl;
+    
+   return 0;
+}
+
+// Write a program that reads two integers: X and Z (Z must be read as many times as necessary, until a number greater than X is read). Count how many integers must be summed in sequence (starting at and including X) so that the sum exceeds Z the minimum possible and writes this number.
+
+// The input may have, for example, the numbers ​​21 21 15 30. In this case, the number 21 is assumed for X, The numbers 21 and 15 must be ignored because they are smaller or equal to X. The number 30 is within the specification (greater than X) and is valid. So, the final result must be 2 for this test case, because the sum (21 + 22) is bigger than 30.
+
+// .Input:
+// The input contains only integer values​​, one per line, which may be positive or negative. The first number is the value of X. The next line will contain Z. If Z does not meet the specification of the problem, it should be read again, as many times as necessary.
+
+// .Output:
+// Print a line with an integer number representing the among of integer numbers that must be summed.
+
+// Input Sample:
+// 3
+// 1
+// 20
+
+// Output Sample:
+// 5

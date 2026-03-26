991f139c51554cb1e7899cc2096d5f47bcfc5aa6
ErickMendes21 <erickmendesrocha15@gmail.com>
2026-03-26 19:44:39 -0300

    1019 - Time Conversion

---
+++ Beginner/1019 - Time Conversion.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std;
+
+void timeConversion(int temp) {
+    int h = temp / 3600;
+    int m = temp / 60;
+    m = m % 60;
+    int s = temp % 60;
+    cout << h << ":" << m << ":" << s << endl;
+}
+
+int main() {
+    int N;
+    cin >> N;
+
+    timeConversion(N);
+
+    return 0;
+}
+
+// Read an integer value, which is the duration in seconds of a certain event in a factory, and inform it expressed in hours:minutes:seconds.
+
+// .Input:
+// The input file contains an integer N.
+
+// .Output:
+// Print the read time in the input file (seconds) converted in hours:minutes:seconds like the following example.
+
+// Input Sample:
+// 556
+// 1
+// 140153
+
+// Output Sample:
+// 0:9:16
+// 0:0:1
+// 38:55:53

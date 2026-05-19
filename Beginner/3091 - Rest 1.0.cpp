fa5129c7afd1738048a1ebfd77f2963dc52ddaf4
ErickMendes21 <erickmendesrocha15@gmail.com>
2026-05-19 19:28:35 -0300

    3091 - Rest 1.0

---
+++ Beginner/3091 - Rest 1.0.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b;
+    cin >> a >> b;
+    cout << a % b << endl;
+
+    return 0;
+}
+
+// John is a math student, but few people know that. One day, one of his classmates, thinking that John did not know basic math concepts, 
+// challenged him to perform a simple calculation which was: Calculate the rest of the division of A ÷ B.
+
+// Since John is very shy and you are best friends, he informed you what the answer to the problem was and asked you to pass this
+// information on to this colleague who challenged him. You can do it ?
+
+// Given two integers A and B, tell what is the rest of division of A ÷ B John passed you.
+
+// .Input:
+// The input contains two integers A and B (1 <= A, B <= 100000).
+
+// .Output:
+// The output contains one unique integer that is the rest of division of A ÷ B.
+
+// Input Samples:
+// 8
+// 3
+
+// Output Samples:
+// 2

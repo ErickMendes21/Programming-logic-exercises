45db9bbca63e527c4f9bdb5f05ba7e1f6aa76dde
ErickMendes21 <erickmendesrocha15@gmail.com>
2026-06-20 21:35:18 -0300

    2717 - Elf Time.js

---
+++ JavaScript/BEGINNER/2717 - Elf Time.js
@@ -0,0 +1,43 @@
+// var input = require('fs').readFileSync('stdin', 'utf8')
+var input = require('fs').readFileSync('/dev/stdin', 'utf8')
+var lines = input.split('\n')
+
+let N = parseInt(lines[0])
+
+let [A, B] = lines[1].split(' ').map(Number)
+
+if (A + B > N) {
+    console.log("Deixa para amanha!")
+} else {
+    console.log("Farei hoje!")
+}
+
+// The manufacturing of Christmas gifts is a very complicated task. Several times the leprechauns stay up late working so that everything can be done in time and with 
+// perfection.
+
+// To better manage their schedule, the leprechauns stipulated how many minutes it takes to manufacture 
+// each gift.
+
+// Its almost the end of the day, and one of the leprechauns asked for your help.
+
+// There're N minutes left for the time to go away, and there are two remaining gifts for elf Ed to manufacture. Help him to find out if he will manage to manufacture both gifts today, or if he should delay the work for the next day.
+
+// Input:
+// Each test case starts with a integer N, indicating how many minutes there are until the time to go away (2 <= N <= 100).
+
+// Following there will be two integers A and B, indicating how many minutes it takes to manufacture each of both gifts Ed has to manufacture (1 <= A, B <= 100).
+
+// Output:
+// Print one row, containing the statement "Farei hoje!" if it's possible to manufacture both gifts before the time to go away, or "Deixa para amanha!" otherwise.
+
+// .Input Samples:
+// 20
+// 15 6
+
+// 20
+// 10 10
+
+// .Output Samples:
+// Deixa para amanha!
+
+// Farei hoje!
